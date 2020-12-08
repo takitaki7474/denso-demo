75bb10114afd27ec97b3bf4ca795c29df9356929
oota <shark747474@gmail.com>
2020-12-08 22:19:44 +0900

    Add process for selecting number of train data

--- demo/modules/argument.py
+++ demo/modules/argument.py
@@ -2,6 +2,7 @@ import argparse
 
 def get_args():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--dataN', '-n', type=int, default=300, help='number of train data (default: 300)')
     parser.add_argument('--epochs', '-e', type=int, default=10, help='number of epochs to train (default: 10)')
     parser.add_argument('--batch_size', '-b', type=int, default=8, help='input batch size for training (default: 8)')
     parser.add_argument('--lr', '-lr', type=float, default=0.01, help='learning rate (default: 0.01)')
